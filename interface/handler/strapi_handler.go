1d4f9611160d77bceee49d132368c8700b77962a
yumekiti <yumekiti120440@gmail.com>
2023-10-15 20:01:23 +0900

    `refactoring`

--- interface/handler/strapi_handler.go
+++ interface/handler/strapi_handler.go
@@ -4,8 +4,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
-	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -23,9 +21,9 @@ func NewStrapiHandler() StrapiHandler {
 	return &strapiHandler{}
 }
 
-func (h *strapiHandler) GetHandler(c echo.Context) error {
-	StrapiURL := "https://cocoroiki-strapi.yumekiti.net"
+const StrapiURL = "http://cocoroiki-cms:1337"
 
+func (h *strapiHandler) GetHandler(c echo.Context) error {
 	q := c.Request().URL.Query()
 	q.Add("populate", "*")
 	c.Request().URL.RawQuery = q.Encode()
@@ -41,49 +39,10 @@ func (h *strapiHandler) GetHandler(c echo.Context) error {
 		log.Fatal(err)
 	}
 
-	if c.Request().URL.Path == "/api/posts" {
-		// Parse the response data
-		var responseData map[string]interface{}
-		err = json.Unmarshal(body, &responseData)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Update createdAt and updatedAt fields
-		data := responseData["data"].([]interface{})
-		for _, v := range data {
-			attributes := v.(map[string]interface{})["attributes"].(map[string]interface{})
-			// add 9 hours to createdAt and updatedAt
-			createdAt := attributes["createdAt"].(string)
-			t, err := time.Parse(time.RFC3339, createdAt)
-			if err != nil {
-				log.Fatal(err)
-			}
-			attributes["createdAt"] = t.Add(9 * time.Hour).Format(time.RFC3339)
-
-			updatedAt := attributes["updatedAt"].(string)
-			t, err = time.Parse(time.RFC3339, updatedAt)
-			if err != nil {
-				log.Fatal(err)
-			}
-			attributes["updatedAt"] = t.Add(9 * time.Hour).Format(time.RFC3339)
-		}
-
-		// Convert the updated data back to JSON
-		updatedBody, err := json.Marshal(responseData)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return c.JSONBlob(req.StatusCode, updatedBody)
-	}
-
 	return c.JSONBlob(req.StatusCode, body)
 }
 
 func (h *strapiHandler) PostHandler(c echo.Context) error {
-	StrapiURL := "https://cocoroiki-strapi.yumekiti.net"
-
 	req, err := http.Post(StrapiURL+c.Request().URL.Path, "application/json", c.Request().Body)
 	if err != nil {
 		log.Fatal(err)
@@ -99,8 +58,6 @@ func (h *strapiHandler) PostHandler(c echo.Context) error {
 }
 
 func (h *strapiHandler) PutHandler(c echo.Context) error {
-	StrapiURL := "https://cocoroiki-strapi.yumekiti.net"
-
 	req, err := http.NewRequest(http.MethodPut, StrapiURL+c.Request().URL.Path, c.Request().Body)
 	if err != nil {
 		log.Fatal(err)
@@ -125,8 +82,6 @@ func (h *strapiHandler) PutHandler(c echo.Context) error {
 
 
 func (h *strapiHandler) DeleteHandler(c echo.Context) error {
-	StrapiURL := "https://cocoroiki-strapi.yumekiti.net"
-
 	req, err := http.NewRequest(http.MethodDelete, StrapiURL+c.Request().URL.Path, c.Request().Body)
 	if err != nil {
 		log.Fatal(err)
